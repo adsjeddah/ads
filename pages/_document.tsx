2c0e2cd12b0d4d56a572477ae3d7546cdac9eba0
adsjeddah <ahmed@example.com>
2025-11-27 01:17:18 +0200

    chore: تحديث GTM ID إلى GTM-NVDNTSRV

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P324S29Q');`,
+})(window,document,'script','dataLayer','GTM-NVDNTSRV');`,
           }}
         />
         {/* End Google Tag Manager */}
@@ -45,7 +45,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P324S29Q"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NVDNTSRV"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
